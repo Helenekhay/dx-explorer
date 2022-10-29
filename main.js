880ac52cb54bf2009b1e58998b301ac35cce33c6
Helenekhay <helenemieee@gmail.com>
2022-10-29 13:46:22 -0700

    completed destop view

---
+++ main.js
@@ -0,0 +1,8 @@
+
+var comotbody = document.querySelector(".mobile-hamburger")
+var inhaler = document.querySelector(".mobile-links")
+
+function addandremove(){
+    comotbody.classList.toggle('biggie')
+    inhaler.classList.toggle('show-inhaler')
+}
