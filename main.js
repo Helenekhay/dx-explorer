aeff2cf8fa98b3ed68986fc88b2532c5f10bfe17
Helenekhay <helenemieee@gmail.com>
2022-11-01 15:11:17 -0700

    completed for large and small screen

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 
 var comotbody = document.querySelector(".mobile-hamburger")
 var inhaler = document.querySelector(".mobile-links")
+// var backdrops = document.querySelector(".backdrop")
 
 function addandremove(){
     comotbody.classList.toggle('biggie')
     inhaler.classList.toggle('show-inhaler')
+    // backdrops.classList.toggle('show-backdrop')
 }
